60545a062b48a92e2a9e121304b0f5129ca3e9c4
Naresh Madhur <nareshmadhur@gmail.com>
2025-07-29 21:20:57 +0000

    I think its still showing incorrectly - something with the font?

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
+        body: ['"PT Sans"', '"Noto Sans"', 'sans-serif'],
         headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
